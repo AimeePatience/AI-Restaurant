7780d77be25c05e47524742e88928f3bc9f91ce5
Raphael Spoerri <coolrustacean@gmail.com>
2025-11-17 19:22:32 -0500

    debugging

--- AIRestaurant/urls.py
+++ AIRestaurant/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('index/', views.home, name='index'),
     path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
-
-    path('ai_chat/', lambda request: render(request, 'ai_chat.html'), name='ai_chat'),
     path('ai_chat/?query=<str:question>/', views.ai_chat),
+    path('ai_chat/', lambda request: render(request, 'ai_chat.html'), name='ai_chat'),
+
     template('cart'),
     template('order_history'),
     template('deposit'),
